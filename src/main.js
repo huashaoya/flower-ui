1ffc9e790bdfb47b4b9e35be5c9f643e8480e0ba
Your Name <you@example.com>
2023-02-10 23:48:22 +0800

    init

--- src/main.js
+++ src/main.js
@@ -4,3 +4,10 @@ import router from './router'
 import store from './store'
 
 createApp(App).use(store).use(router).mount('#app')
+
+const obj = {
+  name: 'huashao', // 6666
+  age: 100
+}
+
+console.log(obj)

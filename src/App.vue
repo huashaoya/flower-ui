08c161fd8f5eb923ed1d269a9a7eec139de499e9
Your Name <you@example.com>
2023-02-11 12:31:54 +0800

    button

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,22 @@
   <div id="app">
     hello flowerUI
     <div>button:</div>
-    <fl-button>登录</fl-button>
-    <fl-button>注册</fl-button>
-    <fl-button>警告</fl-button>
+    <div class="row">
+      <fl-button >默认</fl-button>
+      <fl-button type="primary">提交</fl-button>
+      <fl-button type="success">成功</fl-button>
+      <fl-button type="info">信息</fl-button>
+      <fl-button type="warning">警告</fl-button>
+      <fl-button type="danger">危险</fl-button>
+    </div>
   </div>
 </template>
+
+<style lang="scss">
+.row{
+  margin-bottom: 10px;
+  .fl-button{
+    margin-right: 10px;
+  }
+}
+</style>
--- src/components/button.vue
+++ src/components/button.vue
@@ -1,16 +1,38 @@
 <template>
-    <button class="fl-button">
+    <button class="fl-button" :class="['fl-button--'+type]">
       <span><slot></slot></span>
     </button>
 </template>
 
 <script>
+
 export default {
-  name: 'FlButton'
+  name: 'FlButton',
+  props: {
+    type: {
+      type: String,
+      default: 'default'
+    }
+  },
+  created () {
+    // console.log(this.type)
+  }
 }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
+$normal-color: #fff;
+$normal-active-color: #409eff;
+$primary-color: #409eff;
+$primary-active-color: #66b1ff;
+$info-color: #909399;
+$info-active-color: #a6a9ad;
+$success-color: #67c23a;
+$success-active-color: #85ce61;
+$warning-color: #e6a23c;
+$warning-active-color: #ebb563;
+$danger-color: #f56c6c;
+$danger-active-color: #f78989;
 .fl-button{
   display: inline-block;
   line-height: 1;
@@ -36,6 +58,63 @@ export default {
     color:#409eff;
     border-color:#c6e2ff;
     background-color: #ecf5ff;
+  }
+}
+
+.fl-button--primary {
+  color: #fff;
+  background-color: $primary-color;
+  border-color: $primary-color;
+  //   box-shadow: inset 3px 6px 10px #337ecc, inset -3px -7px 10px #4dbeff;
+  &:hover,
+  &:focus {
+    background: $primary-active-color;
+    background-color: $primary-active-color;
+    color: #fff;
+  }
+}
+.fl-button--success {
+  color: #fff;
+  background-color: $success-color;
+  border-color: $success-color;
+  &:hover,
+  &:focus {
+    background: $success-active-color;
+    background-color: $success-active-color;
+    color: #fff;
+  }
+}
+.fl-button--info {
+  color: #fff;
+  background-color: $info-color;
+  border-color: $info-color;
+  &:hover,
+  &:focus {
+    background: $info-active-color;
+    background-color: $info-active-color;
+    color: #fff;
+  }
+}
+.fl-button--warning {
+  color: #fff;
+  background-color: $warning-color;
+  border-color: $warning-color;
+  &:hover,
+  &:focus {
+    background: $warning-active-color;
+    background-color: $warning-active-color;
+    color: #fff;
+  }
+}
+.fl-button--danger {
+  color: #fff;
+  background-color: $danger-color;
+  border-color: $danger-color;
+  &:hover,
+  &:focus {
+    background: $danger-active-color;
+    background-color: $danger-active-color;
+    color: #fff;
   }
 }
 </style>

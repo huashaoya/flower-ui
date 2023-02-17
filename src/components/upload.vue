cbbb38cfa67d7a5b65dc4cf4553d6f2af429df55
Keanuuuuuu <93373675+Keanuuuuuu@users.noreply.github.com>
2023-02-17 17:37:33 +0800

    update switch

---
+++ src/components/upload.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="fl-upload">
+        <input class="input" type="file" ref="chooseFlie" @change="fileChange">
+        <fl-button @click="handleClick"></fl-button>
+    </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+import FlButton from './button.vue'
+export default {
+  name: 'FlUpload',
+  components: {
+    FlButton
+  }
+}
+</script>
+
+<script setup>
+const chooseFlie = ref()
+function handleClick () {
+  chooseFlie.value.click()
+}
+function fileChange () {
+  console.log(1)
+}
+</script>
+
+<style lang="scss">
+.fl-upload{
+  .input{
+    display: none;
+  }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -9,6 +9,7 @@ import FlInput from './components/input.vue'
 import FlForm from './components/form.vue'
 import FlFormItem from './components/form-item.vue'
 import FlSwitch from './components/switch.vue'
+import FlUpload from './components/upload.vue'
 import './assets/font/iconfont.css'
 // import FlowerUI from '../packages'
 
@@ -21,4 +22,5 @@ app.component(FlInput.name, FlInput)
 app.component(FlForm.name, FlForm)
 app.component(FlFormItem.name, FlFormItem)
 app.component(FlSwitch.name, FlSwitch)
+app.component(FlUpload.name, FlUpload)
 app.mount('#app')

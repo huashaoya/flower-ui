55a29f0e0e357bbd377883a1c231f5e612581b46
huashaoya <3250742495@qq.com>
2023-02-21 12:20:14 +0800

    docs

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,29 @@
-import { defineUserConfig } from 'vuepress'
+import { defaultTheme } from 'vuepress'
 
-export default defineUserConfig({
+export default {
   lang: 'zh-CN',
-  title: '你好， VuePress ！',
-  description: '这是我的第一个 VuePress 站点',
-})
+  title: 'FlowerUI',
+  description: '欢迎来到FlowerUI组件库文档站',
+  head: [
+    ['link', { rel: 'icon', href: '/logo.png' }] // 需要被注入到当前页面的 HTML <head> 中的标签
+  ],
+  theme: defaultTheme({ 
+    logo: '/logo.png',  
+    navbar: [
+      { text: 'Home', link: '/' },
+      // 可指定链接跳转模式：默认target: '_blank'新窗口打开，_self当前窗口打开
+      { text: '百度', link: 'https://www.baidu.com' },
+      { text: 'CSDN', link: 'https://blog.csdn.net', target: '_blank' },
+      { text: '豆瓣', link: 'https://movie.douban.com', target: '_self', rel: '' },
+      {
+        text: 'Group',
+        children: [
+          {
+            text: 'SubGroup',
+            children: ['/group/sub/foo.md', '/group/sub/bar.md'],
+          },
+        ],
+      },
+    ],
+  }),
+}
---
+++ src/components/avator.vue
@@ -0,0 +1,13 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+export default {
+  name: 'FlAvator'
+}
+</script>
+
+<style lang="scss">
+
+</style>

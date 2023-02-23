250c5636ba68e47e0916e99639db7eae9d030e40
huashaoya <3250742495@qq.com>
2023-02-23 20:57:41 +0800

    npm

--- src/main.js
+++ src/main.js
@@ -3,6 +3,6 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import FlowerUI from '../packages'
-
+import '../dist/flowers-ui.css'
 const app = createApp(App).use(store).use(router).use(FlowerUI)
 app.mount('#app')
--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,7 @@ const path = require('path')
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: './', // 關鍵，這樣所有的資源都會被鏈接為相對路徑
   chainWebpack: config => {
     config.module
       .rule('js')
